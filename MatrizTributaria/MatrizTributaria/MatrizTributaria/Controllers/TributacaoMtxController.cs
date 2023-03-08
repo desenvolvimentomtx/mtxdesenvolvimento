b059063fa34856e265057a8f7489e2fc3dbd7022
desenvolvimentomtx <desenvolvimentomtx@gmail.com>
2023-03-08 08:23:24 -0300

    Commit do Projeto inteiro

---
+++ MatrizTributaria/MatrizTributaria/MatrizTributaria/Controllers/TributacaoMtxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatrizTributaria.Controllers
+{
+    public class TributacaoMtxController : Controller
+    {
+        // GET: TributacaoMtx
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
